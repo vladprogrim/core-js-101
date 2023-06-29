b6a13f4e9606db1b8c9edec2afd88f837897796e
vladprogrim <113541340+vladprogrim@users.noreply.github.com>
2023-06-30 01:12:17 +0300

    refactor: 36

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -22,10 +22,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(f, g) {
-  return function (x) {
-    return f(g(x));
-  };
+function getComposition(/* f, g */) {
+  throw new Error('Not implemented');
 }
 
 /**
